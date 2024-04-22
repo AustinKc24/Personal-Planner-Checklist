ce2d3b05dc40fc5bc5f3bd9b315acd23c583fcf9
Austin Kc <ask1047@unh.edu>
2024-04-22 14:03:29 -0400

    trying to fix deployment

--- ui/src/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-    UserPoolId: "us-east-2_oiVdOEQBj",
-    ClientId: "62s908bo98pfcse117m6m8vnuu"
-}
-
-export default new CognitoUserPool(poolData);
